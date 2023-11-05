eec220745a8820ffed1214a776474208004e3254
temirlanemilbekovtu <emilbekov.tute@gmail.com>
2023-11-05 10:52:03 +0600

    latest

--- TelegramBot.cs
+++ TelegramBot.cs
@@ -1,3 +1,4 @@
+using System.Net.Mime;
 using Microsoft.Data.Sqlite;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
@@ -6,6 +7,7 @@ using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 using File = System.IO.File;
+using System;
 
 namespace PurchaseBot;
 
@@ -36,13 +38,6 @@ public class TelegramBot
             cancellationToken: _cts.Token
         );
 
-        await _botClient.SetMyCommandsAsync(
-            new []
-            {
-                new BotCommand { Command = "start", Description = "Начать" }
-            }
-        );
-
         var me = await _botClient.GetMeAsync();
 
         Console.WriteLine($"Start listening for @{me.Username}");
@@ -69,13 +64,23 @@ public class TelegramBot
             return;
         if (callbackQuery.Message is not { } message)
             return;
-        
+
         var chatId = message.Chat.Id;
-        
+
         string command = query.Split(' ')[0];
 
         Console.WriteLine($"Received a '{query}' query in chat {chatId}.");
-
+        
+        try
+        {
+            await _botClient.DeleteMessageAsync(
+                chatId: chatId,
+                messageId: message.MessageId,
+                cancellationToken: cancellationToken
+            );
+        }
+        catch{}
+        
         switch (command)
         {
             case "/to_article":
@@ -85,17 +90,17 @@ public class TelegramBot
                 await HandleHelpCommandAsync(chatId, cancellationToken);
                 break;
         }
-
-        await _botClient.DeleteMessageAsync(chatId, callbackQuery.Message.MessageId, cancellationToken: cancellationToken);
     }
-    
+
     private async Task HandleMessageAsync(Message message, CancellationToken cancellationToken)
     {
         if (message.Text is not { } messageText)
             return;
 
         var chatId = message.Chat.Id;
-        
+
+        messageText = messageText.ToLower();
+
         Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
 
         switch (messageText)
@@ -103,8 +108,8 @@ public class TelegramBot
             case "/start":
                 await HandleStartCommandAsync(chatId, cancellationToken);
                 break;
-            case "Помощь":
-                await HandleHelpCommandAsync(chatId, cancellationToken);
+            case "помощь":
+                await HandleAboutCommandAsync(chatId, cancellationToken);
                 break;
             case "частное лицо":
                 await HandleSetRoleQueryAsync(chatId, "regular", cancellationToken);
@@ -112,29 +117,42 @@ public class TelegramBot
             case "предприниматель":
                 await HandleSetRoleQueryAsync(chatId, "enterpreneur", cancellationToken);
                 break;
+            case "о закупках":
+                await HandleHelpCommandAsync(chatId, cancellationToken);
+                break;
         }
     }
-    
+
     private async Task HandleStartCommandAsync(ChatId chatId, CancellationToken cancellationToken)
     {
         string meetGifUrl = "https://github.com/temirlanemilbekovtu/PurchaseBot/blob/main/anim/meet.gif?raw=true";
 
-        await _botClient.SendAnimationAsync(
-            chatId: chatId,
-            animation: InputFile.FromUri(meetGifUrl),
-            caption: "Привет!", 
-            cancellationToken: cancellationToken
-        );
-
-        SqliteDataReader userReader =
-            await new SqliteCommand($"select 1 from users where user_id = {chatId} is not null;", _db)
+        /*SqliteDataReader userReader =
+            await new SqliteCommand($"select * from users where user_id = {chatId};", _db)
                 .ExecuteReaderAsync(cancellationToken);
 
-        if (!userReader.Read())
+        userReader.Read();
+        
+        if (userReader.HasRows)
         {
-            new SqliteCommand($"insert into users (user_id) values ({chatId});", _db).ExecuteNonQuery();
+            SqliteCommand command = new SqliteCommand($"insert into users (user_id) values (@chatId);", _db);
+            command.Parameters.AddWithValue("@chatId", chatId);
+            command.ExecuteNonQuery();
+            Console.WriteLine($"added {chatId}");
+        }*/
+
+        try
+        {
+            SqliteCommand command = new SqliteCommand("insert into users (user_id) values (@chatId);", _db);
+            command.Parameters.AddWithValue("@chatId", chatId.Identifier);
+            command.ExecuteNonQuery();
         }
-        if (string.IsNullOrEmpty(await GetUserRoleAsync(chatId)))
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        if (string.IsNullOrEmpty(await GetUserRoleAsync(chatId, cancellationToken)))
         {
             ReplyKeyboardMarkup setRoleKeyboard = new(
                 new[]
@@ -164,41 +182,45 @@ public class TelegramBot
                 },
                 new[]
                 {
+                    new KeyboardButton("О закупках")
+                },
+                new[]
+                {
                     new KeyboardButton("Помощь"),
                     new KeyboardButton("Перейти к сайту")
                 }
             }
-        );
+        ) { ResizeKeyboard = true, OneTimeKeyboard = true };
 
         await _botClient.SendTextMessageAsync(
             chatId: chatId,
             text: "Ну-с, добро пожаловать",
-            replyMarkup: mainMenuKeyboard, 
+            replyMarkup: mainMenuKeyboard,
             cancellationToken: cancellationToken
         );
     }
 
     private async Task HandleHelpCommandAsync(ChatId chatId, CancellationToken cancellationToken)
     {
-        string userRole = await GetUserRoleAsync(chatId);
+        string userRole = await GetUserRoleAsync(chatId, cancellationToken);
         SqliteDataReader articlesReader = await GetArticlesAsync(userRole);
-        
+
         int articlesNum = await GetArticlesCountAsync(userRole);
-        
+
         int[] articlesId = new int[articlesNum];
         string replyText = "Инфа: \n\n";
-        
+
         for (int i = 0; articlesReader.Read(); i++)
         {
             var title = articlesReader["title"];
             var articleId = articlesReader["article_id"];
             replyText += $"{i + 1}. {title}\n\n";
-            articlesId[i] = (int) articleId;
+            articlesId[i] = (int)(long)articleId;
         }
 
-        int columnsNum = 2;
+        int columnsNum = 3;
         int rowsNum = articlesNum / columnsNum + (articlesNum % columnsNum == 0 ? 0 : 1);
-        
+
         InlineKeyboardButton[][] helpMenuButtons = new InlineKeyboardButton[rowsNum + 1][];
 
         for (int i = 0; i < articlesNum; i++)
@@ -211,15 +233,15 @@ public class TelegramBot
                 helpMenuButtons[row] = new InlineKeyboardButton[rowLength];
             }
 
-            helpMenuButtons[row][i % columnsNum] = 
+            helpMenuButtons[row][i % columnsNum] =
                 InlineKeyboardButton.WithCallbackData(
-                    text: $"{i + 1}", 
-                    callbackData:$"/to_article {articlesId[i]} {i + 1} {userRole}"
+                    text: $"{i + 1}",
+                    callbackData: $"/to_article {articlesId[i]} {i + 1} {userRole}"
                 );
         }
 
-        helpMenuButtons[^1] = 
-            new[] { InlineKeyboardButton.WithCallbackData(text: "Назад", callbackData: $"/to_help") };
+        helpMenuButtons[^1] =
+            new[] { InlineKeyboardButton.WithCallbackData(text: "Назад", callbackData: $"/start") };
 
         InlineKeyboardMarkup helpMenuKeyboard = new(helpMenuButtons);
 
@@ -227,6 +249,18 @@ public class TelegramBot
             chatId: chatId,
             text: replyText,
             replyMarkup: helpMenuKeyboard,
+            cancellationToken: cancellationToken
+        );
+    }
+
+    private async Task HandleAboutCommandAsync(ChatId chatId, CancellationToken cancellationToken)
+    {
+        string text = "О нас:/n";
+        text = "/nВы можете:";
+
+        await _botClient.SendTextMessageAsync(
+            chatId: chatId,
+            text: text,
             cancellationToken: cancellationToken
         );
     }
@@ -277,7 +311,7 @@ public class TelegramBot
             SqliteDataReader articlePathReader =
                 await new SqliteCommand($"select content_path from articles where article_id = {articleId};", _db)
                     .ExecuteReaderAsync(cancellationToken);
-            
+
             articlePathReader.Read();
 
             content = await File.ReadAllTextAsync(articlePathReader.GetString(0), cancellationToken);
@@ -294,7 +328,7 @@ public class TelegramBot
                     {
                         InlineKeyboardButton.WithCallbackData(text: "Назад", callbackData: "/to_help"),
                         InlineKeyboardButton.WithCallbackData(text: ">>",
-                            callbackData: $"to_article {nextId} {args[2] + 1} {args[3]}"
+                            callbackData: $"/to_article {nextId} {articleSn + 1} {args[3]}"
                         )
                     }
                 );
@@ -306,7 +340,7 @@ public class TelegramBot
                     {
                         InlineKeyboardButton.WithCallbackData(
                             text: "<<",
-                            callbackData: $"to_article {prevId} {articleSn - 1} {args[3]}"
+                            callbackData: $"/to_article {prevId} {articleSn - 1} {args[3]}"
                         ),
                         InlineKeyboardButton.WithCallbackData(text: "Назад", callbackData: "/to_help")
                     }
@@ -319,12 +353,12 @@ public class TelegramBot
                     {
                         InlineKeyboardButton.WithCallbackData(
                             text: "<<",
-                            callbackData: $"to_article {prevId} {articleSn - 1} {args[3]}"
+                            callbackData: $"/to_article {prevId} {articleSn - 1} {args[3]}"
                         ),
                         InlineKeyboardButton.WithCallbackData(text: "Назад", callbackData: "/to_help"),
                         InlineKeyboardButton.WithCallbackData(
                             text: ">>",
-                            callbackData: $"to_article {nextId} {args[2] + 1} {args[3]}"
+                            callbackData: $"/to_article {nextId} {articleSn + 1} {args[3]}"
                         )
                     }
                 );
@@ -344,63 +378,87 @@ public class TelegramBot
         );
     }
 
-    private async Task<string> GetUserRoleAsync(ChatId chatId)
+    private async Task<string> GetUserRoleAsync(ChatId chatId, CancellationToken cancellationToken)
     {
-        SqliteDataReader? roleReader;
-
         try
         {
-            roleReader =
-                await new SqliteCommand($"select user_role from users where user_id = {chatId};", _db)
-                    .ExecuteReaderAsync();
+            var roleReader = await new SqliteCommand($"select user_role from users where user_id = {chatId};", _db)
+                .ExecuteReaderAsync(cancellationToken);
 
             roleReader.Read();
+
+            var res = roleReader.GetString(0);
+
+            Console.WriteLine("success");
+
+            return res;
         }
-        catch
+        catch (Exception e)
         {
+            Console.WriteLine(e.Message);
             return String.Empty;
         }
-
-        return (string)roleReader.GetValue(0);
     }
 
     private async Task<int> GetNextArticleIdAsync(int id, string userRole)
     {
-        Object? nextObj = 
-            await new SqliteCommand(
-                $"select article_id from articles " +
-                $"where article_id > {id} and user_role = '{userRole}' asc limit 1;", _db)
-            .ExecuteScalarAsync();
+        try
+        {
+            Object? nextObj =
+                await new SqliteCommand(
+                        $"select article_id from articles " +
+                        $"where article_id > {id} and user_role = '{userRole}' order by article_id limit 1;", _db)
+                    .ExecuteScalarAsync();
 
-        return (int)(nextObj ?? -1);
+            return Convert.ToInt32(nextObj ?? id);
+        }
+        catch
+        {
+            return id;
+        }
     }
 
     private async Task<int> GetPreviousArticleIdAsync(int id, string userRole)
     {
-        Object? prevObj = 
-            await new SqliteCommand(
-                    $"select article_id from articles " +
-                    $"where article_id < {id} and user_role = '{userRole}' asc limit 1;", _db)
-                .ExecuteScalarAsync();
-
-        return (int)(prevObj ?? -1);
+        try
+        {
+            Object? prevObj =
+                await new SqliteCommand(
+                        $"select article_id from articles " +
+                        $"where article_id < {id} and user_role = '{userRole}' order by article_id limit 1;", _db)
+                    .ExecuteScalarAsync();
+            
+            return Convert.ToInt32(prevObj ?? id);
+        }
+        catch
+        {
+            return id;
+        }
     }
 
     private async Task<int> GetArticlesCountAsync(string userRole)
     {
-        Object? countObj = 
-            await new SqliteCommand(
-                $"select count(*) from articles where user_role = '{userRole}' asc limit 1;", _db)
-            .ExecuteScalarAsync();
+        try
+        {
+            Object? countObj =
+                await new SqliteCommand(
+                        $"select count(*) from articles where user_role = '{userRole}' order by article_id limit 1;",
+                        _db)
+                    .ExecuteScalarAsync();
 
-        return (int)(countObj ?? 0);
+            return Convert.ToInt32(countObj ?? 0);
+        }
+        catch
+        {
+            return 0;
+        }
     }
 
     private Task<SqliteDataReader> GetArticlesAsync(string userRole)
     {
-        SqliteDataReader reader = 
+        SqliteDataReader reader =
             new SqliteCommand($"select * from articles where user_role = '{userRole}'", _db).ExecuteReader();
-        
+
         return Task.FromResult(reader);
     }
 
@@ -417,10 +475,10 @@ public class TelegramBot
         Console.WriteLine(errorMessage);
         return Task.CompletedTask;
     }
-    
+
     public Task ExitBot()
     {
         _cts.Cancel();
         return Task.CompletedTask;
-    } 
+    }
 }
